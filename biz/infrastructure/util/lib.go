2487b1f04efd2b69ce15afde596c4048786ffff9
fuqihao.foliet <fuqihao.foliet@bytedance.com>
2023-07-26 17:43:14 +0800

    fix: ID->Id

--- biz/infrastructure/util/lib.go
+++ biz/infrastructure/util/lib.go
@@ -15,9 +15,9 @@ func JSONF(v any) string {
 }
 
 func ParseInt(s string) int64 {
-	appID, err := strconv.ParseInt(s, 10, 64)
+	i, err := strconv.ParseInt(s, 10, 64)
 	if err != nil {
 		return 0
 	}
-	return appID
+	return i
 }
